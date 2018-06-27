41596feab00200432e4c96fc761b0c3e976028bd
Matthew Choi <mattjchoi@gmail.com>
2018-06-26 22:27:01 -0700

    Init production branch

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Map from './Map.jsx';
-import { API } from '../../config.js';
 
 const Main = (props) => {
   return (
     <div>
       <Map
         locations={props.mentorLocations} // pass in geolocations props
-        googleMapURL={API.googleMapUrl}
+        googleMapURL={process.env.GOOGLE_MAP_URL}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%`, width: `50%` }} />}
